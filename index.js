e95f438b143c84a09324e5ec239a00985e1005ea
yoh <yohaadev@gmail.com>
2026-02-07 12:00:21 -0300

    ajustar

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 
 //express.js node
 const express = require('express')
+const cors require("cors");
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.use(cors());
+
+app.get('/users', (request, response) => {
+    response.json({nome: "teste", idade:90});
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`App rodando em: http://localhost:${port}`)
 })
